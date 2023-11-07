ce11648ac2d4dbc94cf96e080a562fd19bcf81b8
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-11-07 14:13:20 +0100

    404 changes

--- js.js
+++ js.js
@@ -1,14 +1,11 @@
-
-
-
 //carousel NOT WORKING YET
 
-document.addEventListener("DOMContentLoaded", function() {
-    var carouselTrack = document.querySelector('.carousel-track');
-    new Dragdealer(carouselTrack, {
-        steps: 3,
-        speed: 0.3,
-        loose: true,
-        requestAnimationFrame: true
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var carouselTrack = document.querySelector(".carousel-track");
+  new Dragdealer(carouselTrack, {
+    steps: 3,
+    speed: 0.3,
+    loose: true,
+    requestAnimationFrame: true,
+  });
 });
